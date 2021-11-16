884a2b3a38bab15ed0c99d42363d8fd5d171c603
unknown <gergokiss04@gmail.com>
2021-11-16 13:38:02 +0100

    Hetedik mentés

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -28,10 +28,27 @@ print(db)
 #Feltételes összegzés:Csak a páros számokat összegzem.
 osszeg=0
 for x in szamok:
-    if (x%2==0):
+    if (x%2==0): 
         osszeg=osszeg+x;
 print("Páros számok összege: ")
 print(osszeg)
 
+#Minimum kiválasztás tétele:
+print("Minimum kiválasztás tétele:")
+min1=1000
+for x in szamok:
+    if (x<min1):                  
+        min1=x   
+print(min1)       
+
+print(min(szamok))
 
 
+#Maximum kiválasztás tétele:
+print("Maximum kiválasztás tétele:")
+max1=-1000
+for x in szamok:
+    if (x>max1):
+        max1=x
+print(max1)
+print(max(szamok))
