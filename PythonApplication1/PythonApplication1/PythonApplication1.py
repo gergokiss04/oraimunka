8509b6dcaf0f41d9634caf882fc828a3411b56bb
unknown <gergokiss04@gmail.com>
2021-11-16 12:10:59 +0100

    Negyedik mentés

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,3 +1,9 @@
-print("Működik a git ? ")
-print("Mindjárt kiderül")
-print("Remélem működik")
+szamok=[3,4,2,7,8,1,9,7,3]
+print(szamok)
+
+
+#összegzés tétele
+osszeg=0
+for x in szamok:
+    osszeg=osszeg+x    
+print(osszeg)
