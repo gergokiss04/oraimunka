589f613e458dcaa1d51ea8ab5a6adb19b6d3871b
unknown <gergokiss04@gmail.com>
2021-11-16 12:18:07 +0100

    Ötödik mentés

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -7,3 +7,12 @@ osszeg=0
 for x in szamok:
     osszeg=osszeg+x    
 print(osszeg)
+
+print(sum(szamok))
+
+#eldöntés tétele
+vane=False
+for x in szamok:
+    if(x==1):
+        vane=True
+print(vane)
